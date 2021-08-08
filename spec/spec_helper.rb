14852fb8affa6586a13c3803cf039222405ecccf
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-08-07 20:07:16 -0400

    Add coveralls_reborn to RSpec and GitHub actions
    Add coveralls_reborn Gem fork
    (https://github.com/tagliala/coveralls-ruby-reborn) as a development
    dependency to the repo to allow for Coveralls open source to work on the
    repo. This is currently better maintained than the official coveralls
    gem (https://github.com/lemurheavy/coveralls-ruby).
    
    Also updated bundle to account for a patched version of rexml.
    
    Completes #11 Add in a code coverage plugin

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.start do
+  if ENV["CI"]
+    require "simplecov-lcov"
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = "coverage/lcov.info"
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+end
+
 require "bundler/setup"
 require "nerd_dice"
 
