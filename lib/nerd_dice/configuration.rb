13170dea4ded3205ddd122f275eec4fb8a1633be
Michael Duchemin <msducheminjr@users.noreply.github.com>
2021-09-04 19:55:58 -0400

    Modify Configuration for rolling of ability scores
    Add validations to the existing ability_score_array_size attribute in
    Configuration to use .to_i and duck-typing as well as validate that the
    configured value is a positive integer.
    
    Add 3 new attributes to Configuration related to rolling ability scores
    * ability_score_number_of_sides: default 6, must be positive Integer
    * ability_score_dice_rolled: default 4, must be a positive Integer
    * ability_score_dice_kept: default 3, must be a positive Integer less
      than or equal to ability_score_dice_rolled
    
    In the event that ability_score_dice_kept is set higher than the
    existing ability_score_dice_rolled, it will raise an error. In the event
    that ability_score_dice_rolled is set lower than the existing
    ability_score_dice_kept, it will warn and set the value of
    ability_score_dice_kept to match ability_score_dice_rolled
    
    Completes NerdDice backlog item:
    Modify Configuration for roll ability scores

--- lib/nerd_dice/configuration.rb
+++ lib/nerd_dice/configuration.rb
@@ -18,8 +18,9 @@ module NerdDice
   #   You can also set a particular property without a block using inline assignment
   #   <tt>NerdDice.configuration.randomization_technique = :random_new_once</tt>
   class Configuration
-    attr_reader :randomization_technique, :refresh_seed_interval
-    attr_accessor :ability_score_array_size, :die_background_color, :die_foreground_color
+    attr_reader :randomization_technique, :refresh_seed_interval, :ability_score_array_size,
+                :ability_score_dice_rolled, :ability_score_number_of_sides, :ability_score_dice_kept
+    attr_accessor :die_background_color, :die_foreground_color
 
     DEFAULT_BACKGROUND_COLOR = "#0000DD"
     DEFAULT_FOREGROUND_COLOR = "#DDDDDD"
@@ -40,10 +41,63 @@ module NerdDice
       @refresh_seed_interval = value
     end
 
+    def ability_score_array_size=(value)
+      error_message = "ability_score_array_size must be must be a positive value that responds to :to_i"
+      new_value = value.to_i
+      raise ArgumentError, error_message unless new_value.positive?
+
+      @ability_score_array_size = new_value
+    rescue NoMethodError
+      raise ArgumentError, error_message
+    end
+
+    def ability_score_number_of_sides=(value)
+      error_message = "ability_score_number_of_sides must be must be a positive value that responds to :to_i"
+      new_value = value.to_i
+      raise ArgumentError, error_message unless new_value.positive?
+
+      @ability_score_number_of_sides = new_value
+    rescue NoMethodError
+      raise ArgumentError, error_message
+    end
+
+    # will refactor
+    # rubocop:disable Metrics/MethodLength
+    def ability_score_dice_rolled=(value)
+      error_message = "ability_score_dice_rolled must be must be a positive value that responds to :to_i"
+      new_value = value.to_i
+      raise ArgumentError, error_message unless new_value.positive?
+
+      @ability_score_dice_rolled = new_value
+      if @ability_score_dice_rolled < ability_score_dice_kept
+        warn "WARNING: ability_score_dice_rolled set to lower value than ability_score_dice_kept. " \
+             "Reducing ability_score_dice_kept from #{ability_score_dice_kept} to #{new_value}"
+        self.ability_score_dice_kept = new_value
+      end
+    rescue NoMethodError
+      raise ArgumentError, error_message
+    end
+    # rubocop:enable Metrics/MethodLength
+
+    def ability_score_dice_kept=(value)
+      duck_type_error_message = "ability_score_dice_kept must be must be a positive value that responds to :to_i"
+      compare_error_message = "cannot set ability_score_dice_kept greater than ability_score_dice_rolled"
+      new_value = value.to_i
+      raise ArgumentError, duck_type_error_message unless new_value.positive?
+      raise NerdDice::Error, compare_error_message if new_value > @ability_score_dice_rolled
+
+      @ability_score_dice_kept = new_value
+    rescue NoMethodError
+      raise ArgumentError, duck_type_error_message
+    end
+
     private
 
       def initialize
         @ability_score_array_size = 6
+        @ability_score_number_of_sides = 6
+        @ability_score_dice_rolled = 4
+        @ability_score_dice_kept = 3
         @randomization_technique = :random_object
         @die_background_color = DEFAULT_BACKGROUND_COLOR
         @die_foreground_color = DEFAULT_FOREGROUND_COLOR
--- spec/nerd_dice/configuration_spec.rb
+++ spec/nerd_dice/configuration_spec.rb
@@ -7,6 +7,18 @@ RSpec.describe NerdDice::Configuration do
     expect(config.ability_score_array_size).to eq(6)
   end
 
+  it "has default ability_score_number_of_sides of 6" do
+    expect(config.ability_score_number_of_sides).to eq(6)
+  end
+
+  it "has default ability_score_dice_rolled of 4" do
+    expect(config.ability_score_dice_rolled).to eq(4)
+  end
+
+  it "has default ability_score_dice_kept of 3" do
+    expect(config.ability_score_dice_kept).to eq(3)
+  end
+
   it "has default randomization_technique of :random_object" do
     expect(config.randomization_technique).to eq(:random_object)
   end
@@ -59,5 +71,153 @@ RSpec.describe NerdDice::Configuration do
   it "will let you set the die_foreground_color" do
     config.die_foreground_color = "#00DD00"
     expect(config.die_foreground_color).to eq("#00DD00")
+  end
+
+  describe "duck-type Integer attributes" do
+    describe "ability_score_array_size attribute writer" do
+      it "allows you to set a new array size" do
+        config.ability_score_array_size = 7
+        expect(config.ability_score_array_size).to eq(7)
+      end
+
+      it "allows you to provide a numeric string" do
+        config.ability_score_array_size = "8"
+        expect(config.ability_score_array_size).to eq(8)
+      end
+    end
+
+    describe "ability_score_number_of_sides attribute writer" do
+      it "allows you to set a new number of sides" do
+        config.ability_score_number_of_sides = 8
+        expect(config.ability_score_number_of_sides).to eq(8)
+      end
+
+      it "allows you to provide a numeric string" do
+        config.ability_score_number_of_sides = "4"
+        expect(config.ability_score_number_of_sides).to eq(4)
+      end
+    end
+
+    describe "ability_score_dice_rolled attribute writer" do
+      it "allows you to set a quantity of dice rolled" do
+        config.ability_score_dice_rolled = 5
+        expect(config.ability_score_dice_rolled).to eq(5)
+      end
+
+      it "allows you to provide a numeric string" do
+        config.ability_score_dice_rolled = "3"
+        expect(config.ability_score_dice_rolled).to eq(3)
+      end
+
+      it "allows you to set the value lower than dice kept" do
+        config.ability_score_dice_rolled = 2
+        expect(config.ability_score_dice_rolled).to eq(2)
+      end
+
+      it "reduces dice kept if set lower than dice kept" do
+        config.ability_score_dice_rolled = 2
+        expect(config.ability_score_dice_kept).to eq(2)
+      end
+    end
+
+    describe "ability_score_dice_kept attribute writer" do
+      it "allows you to set a new quantity of dice_kept" do
+        config.ability_score_dice_kept = 2
+        expect(config.ability_score_dice_kept).to eq(2)
+      end
+
+      it "allows you to provide a numeric string" do
+        config.ability_score_dice_kept = "4"
+        expect(config.ability_score_dice_kept).to eq(4)
+      end
+    end
+  end
+
+  describe "validations and error handling" do
+    describe "ability_score_array_size attribute writer errors" do
+      let(:error_message) { "ability_score_array_size must be must be a positive value that responds to :to_i" }
+
+      it "throws an ArgumentError if new value does not respond to to_i" do
+        expect { config.ability_score_array_size = :threeve }.to raise_error(ArgumentError, error_message)
+      end
+
+      it "throws an ArgumentError if new value does not convert to positive integer" do
+        expect { config.ability_score_array_size = "eleventy-billion" }.to raise_error(ArgumentError, error_message)
+      end
+
+      it "throws an ArgumentError if new value is 0" do
+        expect { config.ability_score_array_size = 0 }.to raise_error(ArgumentError, error_message)
+      end
+
+      it "throws an ArgumentError if new value is a negative Integer" do
+        expect { config.ability_score_array_size = -6 }.to raise_error(ArgumentError, error_message)
+      end
+    end
+  end
+
+  describe "ability_score_number_of_sides attribute writer errors" do
+    let(:error_message) { "ability_score_number_of_sides must be must be a positive value that responds to :to_i" }
+
+    it "throws an ArgumentError if new value does not respond to to_i" do
+      expect { config.ability_score_number_of_sides = :threeve }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value does not convert to positive integer" do
+      expect { config.ability_score_number_of_sides = "eleventy-billion" }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is 0" do
+      expect { config.ability_score_number_of_sides = 0 }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is a negative Integer" do
+      expect { config.ability_score_number_of_sides = -6 }.to raise_error(ArgumentError, error_message)
+    end
+  end
+
+  describe "ability_score_dice_rolled attribute writer errors" do
+    let(:error_message) { "ability_score_dice_rolled must be must be a positive value that responds to :to_i" }
+
+    it "throws an ArgumentError if new value does not respond to to_i" do
+      expect { config.ability_score_dice_rolled = :threeve }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value does not convert to positive integer" do
+      expect { config.ability_score_dice_rolled = "eleventy-billion" }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is 0" do
+      expect { config.ability_score_dice_rolled = 0 }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is a negative Integer" do
+      expect { config.ability_score_dice_rolled = -6 }.to raise_error(ArgumentError, error_message)
+    end
+  end
+
+  describe "ability_score_dice_kept attribute writer errors" do
+    let(:error_message) { "ability_score_dice_kept must be must be a positive value that responds to :to_i" }
+
+    it "throws an ArgumentError if new value does not respond to to_i" do
+      expect { config.ability_score_dice_kept = :threeve }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value does not convert to positive integer" do
+      expect { config.ability_score_dice_kept = "eleventy-billion" }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is 0" do
+      expect { config.ability_score_dice_kept = 0 }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an ArgumentError if new value is a negative Integer" do
+      expect { config.ability_score_dice_kept = -6 }.to raise_error(ArgumentError, error_message)
+    end
+
+    it "throws an error if you try to set it greater than ability_score_dice_rolled" do
+      expect { config.ability_score_dice_kept = 12 }.to raise_error(
+        NerdDice::Error, "cannot set ability_score_dice_kept greater than ability_score_dice_rolled"
+      )
+    end
   end
 end
